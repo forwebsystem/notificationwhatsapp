696b19c020ae0d559099ad298d7e424d36eea19e
Joao <jo4o.fds@gmail.com>
2023-07-21 11:05:11 -0300

    remove email do contrato

--- src/Contracts/ReceiverInterface.php
+++ src/Contracts/ReceiverInterface.php
@@ -12,13 +12,6 @@ interface ReceiverInterface
     public function name(): string;
 
     /**
-     * Método que retorna o email do destinatário.
-     *
-     * @return string
-     */
-    public function email(): string;
-
-    /**
      * Método que retorna o telefone do destinatário.
      *
      * @return string
