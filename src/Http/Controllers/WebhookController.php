67f827150f3faedd44411e4cbdf743aeda88782e
For Web System <72976017+forwebsystem@users.noreply.github.com>
2023-11-14 15:43:55 -0300

    Update WebhookController.php
    Caso tenha erro ao receber um notificação informando o cliente

--- src/Http/Controllers/WebhookController.php
+++ src/Http/Controllers/WebhookController.php
@@ -69,25 +69,35 @@ class WebhookController extends Controller
 
     public function fivezapMessage(Request $request, FivezapMessageService $fivezap)
     {
-        $method         = $request->method();
-        $url            = $request->url();
-        $message_type   = $request->message_type;
-        $account_id     = $request->account['id'];
-        $inbox_id       = $request->conversation['inbox_id'];
-        
-        if ($message_type == 'incoming') {
-            // busca a instancia
-            $user = User::where('notificationwhatsapp_license', $account_id)
-            ->where('notificationwhatsapp_token', $inbox_id)
-            ->first();
-
-            $notification = $fivezap->save($method, $url, $message_type, $request->all());
-
-            // preenche objeto de mensagem e dispara evento.
-            if($notification) {
-                $fivezap_message = new NotificationWhatsAppReceived($notification->toArray());
-                event(new NotificationWhatsAppReceivedEvent($fivezap_message, $user));
+        try {
+            $method         = $request->method();
+            $url            = $request->url();
+            $message_type   = $request->message_type;
+            $account_id     = $request->account['id'];
+            $inbox_id       = $request->conversation['inbox_id'];
+            
+            if ($message_type == 'incoming') {
+                // busca a instancia
+                $user = User::where('notificationwhatsapp_license', $account_id)
+                ->where('notificationwhatsapp_token', $inbox_id)
+                ->first();
+    
+                $notification = $fivezap->save($method, $url, $message_type, $request->all());
+    
+                // preenche objeto de mensagem e dispara evento.
+                if($notification) {
+                    $fivezap_message = new NotificationWhatsAppReceived($notification->toArray());
+                    event(new NotificationWhatsAppReceivedEvent($fivezap_message, $user));
+                }
             }
+            
+            return response()->json(['mensagem' => 'Notificação recebida com sucesso']);
+            
+        } catch (\Exception $th) {
+            return response([
+                'code' => 500,
+                'message' => $th->getMessage()
+            ], 500);
         }
     }
 }
